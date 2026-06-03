1180d380556f98acfd827ed482a58e35a748ab30
linhuu9-netizen <linhuu9@gmail.com>
2026-06-03 14:18:15 +0700

    Delete cay-main directory

--- cay-main/src/platform/macos/MacInputInjector.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "CayTypes.h"
-
-namespace CayIME {
-class MacInputInjector {
-public:
-    static void ReplaceText(int backspaceCount, const wchar_t* newText, int newTextLen);
-};
-} // namespace CayIME
